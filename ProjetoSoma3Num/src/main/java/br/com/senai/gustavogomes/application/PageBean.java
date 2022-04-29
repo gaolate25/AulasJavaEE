6b279aec281a7421d4db993de60ad4a31ef88027
aluno <aluno@DSN1251230712>
2022-04-29 16:01:42 -0300

    Cadastro

--- ProjetoSoma3Num/src/main/java/br/com/senai/gustavogomes/application/PageBean.java
+++ ProjetoSoma3Num/src/main/java/br/com/senai/gustavogomes/application/PageBean.java
@@ -3,30 +3,62 @@ package br.com.senai.gustavogomes.application;
 import java.io.Serializable;
 
 import javax.enterprise.context.RequestScoped;
+import javax.faces.context.Flash;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 @SuppressWarnings("serial")
 @Named
 @RequestScoped
 public class PageBean implements Serializable {
-
-	private String page;
-
-	public String getPage() {
-		return page;
-	}
-
-	public void setPage(String page) {
-		this.page = page;
-	}
+	private int n1;
+	private int n2;
+	private int n3;
+	private int resultado;
 	
-	public String goTo() {
-		if("a".equals(page)) {
-			return "page_a";
-		}else if("b".equals(page)) {
-			return "page_b";
-		}else {
-			return null;
-		}
+	
+	public int getResultado() {
+		return resultado;
+	}
+
+	public void setResultado(int resultado) {
+		this.resultado = resultado;
+	}
+
+	public int getN1() {
+		return n1;
+	}
+
+	public void setN1(int n1) {
+		this.n1 = n1;
+	}
+
+	public int getN2() {
+		return n2;
+	}
+
+	public void setN2(int n2) {
+		this.n2 = n2;
+	}
+
+	public int getN3() {
+		return n3;
+	}
+
+	public void setN3(int n3) {
+		this.n3 = n3;
+	}
+
+	
+	private Flash flash;
+	
+	public String processar1() {
+		resultado = n1 + n2 + n3;
+		
+		flash.put("resultado",resultado);
+		
+		return "result?faces-redirect-true";
 	}
 }
+
+
