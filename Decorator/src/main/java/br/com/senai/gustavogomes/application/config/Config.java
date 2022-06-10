3a3f303132789de8c972af1ae7e630c9163713fc
aluno <aluno@DSN1251230709>
2022-06-10 17:09:51 -0300

    decorator

--- Decorator/src/main/java/br/com/senai/gustavogomes/application/config/Config.java
+++ Decorator/src/main/java/br/com/senai/gustavogomes/application/config/Config.java
@@ -5,7 +5,7 @@ import javax.faces.annotation.FacesConfig;
 import javax.faces.annotation.FacesConfig.Version;
 
 @FacesConfig(version = Version.JSF_2_3) //é responsável por descrever os elementos e elementos que compõem o projeto, tais como as regras de navegação, os beans gerenciados, configurações de localização,
-@ApplicationScoped //uma instância será criada apenas uma vez durante todo o sistema
+@ApplicationScoped //uma instância será criada apenas uma vez durante todo o sistema e depois sera apaga 
 public class Config {
 
 }
